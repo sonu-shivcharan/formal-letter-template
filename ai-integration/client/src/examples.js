48bc1491c1c3f74efb6fe0d2bb56c97342643ef1
rampatil119 <rameshwarpatil9689@gmail.com>
2024-12-20 14:16:55 +0530

    add example feature

--- ai-integration/client/src/examples.js
+++ ai-integration/client/src/examples.js
@@ -6,32 +6,33 @@ const sidebar = document.querySelector('.side-bar')
 const exampleContainer = document.querySelector('.example-container')
 const overlay = document.querySelector('.overlay');
 const example = document.querySelector('.example');
-let isDisplayed=false
+let isDisplayed = false
+let exampleLetterData = null
 exampleBtn.addEventListener("click", function () {
-  if(!isDisplayed){
+  if (!isDisplayed) {
     displayExample()
-    isDisplayed=true
+    isDisplayed = true
   }
 
-    console.log('example is ready');
-    sidebar.style.translate = "0"
-    overlay.style.display = "block"
-    overlay.addEventListener("click", function () {
-        overlay.style.display = "none"
-        sidebar.style.translate = "100%"
-    })
+  console.log('example is ready');
+  sidebar.style.translate = "0"
+  overlay.style.display = "block"
+  overlay.addEventListener("click", function () {
+    overlay.style.display = "none"
+    sidebar.style.translate = "100%"
+  })
 
 });
-function closeSideBar(){
-sidebar.style.translate="100%"
-overlay.style.display="none"
+function closeSideBar() {
+  sidebar.style.translate = "100%"
+  overlay.style.display = "none"
 }
 
 function displayExample() {
-    exampleContainer.innerHTML = " "
-    exampleData.forEach(function (data) {
-        console.log(data);
-        exampleContainer.innerHTML += `
+  exampleContainer.innerHTML = " "
+  exampleData.forEach(function (data) {
+    console.log(data);
+    exampleContainer.innerHTML += `
         <div data-id=${data.id} class="example"> 
         <h4> ${data.name}</h4>
         <div class="dis">
@@ -39,23 +40,58 @@ function displayExample() {
         </div>
       </div>`
 
-    })
-    console.log(exampleContainer.children);
-    const children=exampleContainer.children
-    for (let i= 0; i< children.length; i++) {
-       console.log( children[i] );    
-       children[i].addEventListener("click",function(e){
-        closeSideBar()
-        console.log(children[i].dataset.id);
-        fetch("./assets/exampleLetterData.json")
-        .then(() {
-          
-        })
-       })   
-    }
-    
-}
+  })
+  console.log(exampleContainer.children);
+  const children = exampleContainer.children
+  for (let i = 0; i < children.length; i++) {
+    console.log(children[i]);
+    children[i].addEventListener("click", (e) => {
+      closeSideBar()
+      fetchLetterData(children[i])
 
+    })
+  }
+
+}
+function fetchLetterData(target) {
+  let id;
+  if (exampleLetterData == null) {
+    fetch("../public/exampleLetterData.json")
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        exampleLetterData = data.data
+        id=target.dataset.id
+        insertValues(findData(id))
+      })
+      .catch((error) => console.log(error));
+  }
+  else {
+   id=target.dataset.id
+    insertValues(findData(id))
+  }
+
+
+}
+function findData(id){
+  console.log("findData");
+  let temp;
+  exampleLetterData.forEach(element => {
+    console.log(element);
+    if (element.id==id) {
+      temp=element.data;
+    }
+  });
+  return temp
+}
+function insertValues(letterData) {
+  console.log("hugbgh",letterData);
+  for (const key in letterData) {
+    const field = document.getElementById(key.toString());
+    field.value = letterData[key];
+  }
+}
 
 /*  
   
